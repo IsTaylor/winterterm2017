d5d662c37fda3dbb185025d2f7e75e21a1ef0794
Isabel Taylor <birdysbox2239@gmail.com>
2017-05-02 15:34:04 -0400

    updated README; MidiConv now handles multiple files

--- MidiConv.java
+++ MidiConv.java
@@ -1,55 +1,112 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
 public class MidiConv {
-    
+
     private static int TEMPO = 500000;
     private static int BPM = 60000000/TEMPO;
     private static int STEP = 256 / 4;
+    private static int fileCounter = 0;
 
     public static void main(String[] args) {
-	Scanner scanny = null;
-	try {
-	    scanny = new Scanner(new File("/Users/qbp/Dropbox/WT 2017/test.csv"));
-	} catch (FileNotFoundException e) {
-	    System.out.println("Not a valid file:" + e.getMessage());
-	    System.exit(1);
-	}
+      String myDirectoryPath = args[0];
+      
+      File in_dir = new File("./" + myDirectoryPath);
+      File[] directoryListing = in_dir.listFiles();
+      if (directoryListing != null) {
+    	  
+    	//Make a directory to put converted files into
+		  File out_dir = new File("./tensorflow_input");
+		  if (!out_dir.exists()) {
+		      if (out_dir.mkdir()) {
+		          System.out.println("Directory is created!");
+		      } else {
+		          System.out.println("Failed to create directory!");
+		      }
+		  }
+    	  
+    	Scanner scanny = null;
+        for (File child : directoryListing) {
+        	
+      	//open up a new scanner with the child
+	    	try {
+	    	    scanny = new Scanner(child);
+	    	} catch (FileNotFoundException e) {
+	    	    System.out.println("Not a valid file:" + e.getMessage());
+	    	    continue;
+	    	}
+	    	
+	    //scan through the child
+	    	try{
+	    		ArrayList<MidiArray> arrayArray = reader(scanny);
+	    		
+	    		//output stuff to new files and directories
+	    		try{
+	    			System.out.println("Made file");
+	    			File fout = new File(out_dir + "/" + fileCounter + "-tf.txt");
+	    			fileCounter ++;
+	    			
+	    			FileOutputStream fos = new FileOutputStream(fout); 
+	    			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
 	
-	ArrayList<MidiArray> arrayArray = reader(scanny);
-	scanny.close();
-	
-	for (int i = 0; i < arrayArray.size(); i++) {
-	    System.out.println(arrayArray.get(i).toString());
-	}
+	    			for (int i = 0; i < arrayArray.size(); i++) {
+		    	    	bw.write(arrayArray.get(i).toString());
+		    		}
+	    			
+		    	
+	    			
+	    		} catch (Exception e) {
+	    		   System.out.println("I couldn't make the file :( " + e);
+	    		}
+	    		
+	    		
+	    		
+	    		
+	    		
+	    	} catch (Exception e){
+	    		System.out.println("Encounterd a non-midi file: " + child);
+	    	}
+	    
+        }
+        scanny.close();
+      } else {
+    	System.out.println("Not a valid directory, please try again");
 
+	    System.exit(0);
+      }
     }
     
     private static ArrayList<MidiArray> reader(Scanner scanny) {
 	ArrayList<MidiArray> arrayArray = new ArrayList<MidiArray>();
-	
+
 	int time = 0;
-	
+
 	MidiArray startArray = new MidiArray(0);
 	arrayArray.add(startArray);
-	
+
 	while (scanny.hasNextLine()) {
 	    String[] currentEvent = parseLine(scanny.nextLine());
 	    int track = Integer.parseInt(currentEvent[0]);
 	    int currentTime = Integer.parseInt(currentEvent[1]);
-	    
+
 	    //keeps track of tempo as a a global variable
 	    if (currentEvent[2].equals("Tempo") && currentEvent[1].equals("0")) {
 		TEMPO = Integer.parseInt(currentEvent[3]);
 	    }
-	    
+
 	  //keeps track of tempo as a a global variable
 	    if (currentEvent[2].equals("Header")) {
 		STEP = Integer.parseInt(currentEvent[5]) / 4;
 	    }
-	    
+
 	    if (track == 2) {
 		if (currentTime != time) {
 		    time = currentTime;
@@ -58,22 +115,23 @@ public class MidiConv {
 		    for(int j = 0; j < pastNotes.length; j++){
 			midiObject.notes[j] = pastNotes[j];
 		    }
-		 
+
 		    arrayArray.add(midiObject);
 		}
-		
-		for (int i = 0; i < currentEvent.length; i++) {
+
+		/*for (int i = 0; i < currentEvent.length; i++) {
 		    System.out.print(currentEvent[i] + " ");
 		}
 		System.out.println("\n");
-		
-		
+		*/
+
+
 		if (currentEvent[2].equals("Note_on_c")){
 		    Integer[] currentNotes = arrayArray.get(arrayArray.size() - 1).notes;
 		    int midiValue = Integer.parseInt(currentEvent[4]);
 		    currentNotes[midiValue] = 1;
-		    System.out.println("note on");
-		} 
+		    //System.out.println("note on");
+		}
 		if (currentEvent[2].equals("Note_off_c")){
 		    Integer[] currentNotes = arrayArray.get(arrayArray.size() - 1).notes;
 		    int midiValue = Integer.parseInt(currentEvent[4]);
@@ -83,16 +141,16 @@ public class MidiConv {
 	}
 	return arrayArray;
     }
-    
+
     private static String[] parseLine(String line) {
 	String[] parts = line.split(",\\s+");
 	return parts;
     }
-    
+
     public static class MidiArray {
 	public Integer[] notes;
-	public int timing; 
-	
+	public int timing;
+
 	public MidiArray(int time) {
 	    this.notes = new Integer[128];
 	    for (int i = 0; i < notes.length; i++ ){
@@ -100,7 +158,7 @@ public class MidiConv {
 	    }
 	    this.timing = time;
 	}
-	
+
 	public String toString() {
 	    String retStr = "" + this.timing + " [";
 	    for (int i = 0; i < notes.length; i++) {
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # winterterm2017
 Attempting machine learning
+
+## Getting Started
+
+### Prerequisites
+If you want to train with your own midi files, you'll want to download Midicsv(http://www.fourmilab.ch/webtools/midicsv/).
+This website has a tar file about halfway down, along with usage information. Once you have downloaded it, follow the instructions in their README, go ahead and test it out if you want.
+
+### Installing
+Pull from github, but the only files you'll really care about are MidiConv.java and tf-test.py
+
+MidiConv - Despite the misleading name, this converts csv files into tensorflow-compatible files. It is optimized for large file sets.
+	Prereqs:
+		1. A folder with all the converted csv files in the same directory as MidiConv
+	Usage:
+		java MidiConv input_dir (where input_dir is the name of your folder without the "./" So if my folder were "./csvIn", all you would type is "csvIn")
+	Outputs:
+		MidiConv outputs a folder called tensorflow_input. In this folder are a list of converted files, with titles in the format of "#-tf.txt", where number is the order in which the files were seen (ie 0-tf.txt, 1-tf.txt, 2-tf.txt). these are ready to be used to train your network
+		
+		
