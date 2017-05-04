249b1795317117384d1f2818024bdd2aea9ac066
Quinn BP <qbarkerp@gmail.com>
2017-05-03 20:29:37 -0400

    Add files via upload

--- MidiConv.java
+++ MidiConv.java
@@ -1,90 +1,33 @@
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.io.IOException;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
 
 public class MidiConv {
 
     private static int TEMPO = 500000;
     private static int BPM = 60000000/TEMPO;
     private static int STEP = 256 / 4;
-    private static int fileCounter = 0;
 
-    public static void main(String[] args) {
-      String myDirectoryPath = args[0];
-      
-      File in_dir = new File("./" + myDirectoryPath);
-      File[] directoryListing = in_dir.listFiles();
-      if (directoryListing != null) {
-    	  
-    	//Make a directory to put converted files into
-		  File out_dir = new File("./tensorflow_input");
-		  if (!out_dir.exists()) {
-		      if (out_dir.mkdir()) {
-		          System.out.println("Directory is created!");
-		      } else {
-		          System.out.println("Failed to create directory!");
-		      }
-		  }
-    	  
-    	Scanner scanny = null;
-        for (File child : directoryListing) {
-        	
-      	//open up a new scanner with the child
-	    	try {
-	    	    scanny = new Scanner(child);
-	    	} catch (FileNotFoundException e) {
-	    	    System.out.println("Not a valid file:" + e.getMessage());
-	    	    continue;
-	    	}
-	    	
-	    //scan through the child
-	    	try{
-	    		ArrayList<MidiArray> arrayArray = reader(scanny);
-	    		
-	    		//output stuff to new files and directories
-	    		try{
-	    			System.out.println("Made file");
-	    			File fout = new File(out_dir + "/" + fileCounter + "-tf.txt");
-	    			fileCounter ++;
-	    			
-	    			FileOutputStream fos = new FileOutputStream(fout); 
-	    			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
-	
-	    			for (int i = 0; i < arrayArray.size(); i++) {
-		    	    	bw.write(arrayArray.get(i).toString());
-		    		}
-	    			
-		    	
-	    			
-	    		} catch (Exception e) {
-	    		   System.out.println("I couldn't make the file :( " + e);
-	    		}
-	    		
-	    		
-	    		
-	    		
-	    		
-	    	} catch (Exception e){
-	    		System.out.println("Encounterd a non-midi file: " + child);
-	    	}
-	    
-        }
-        scanny.close();
-      } else {
-    	System.out.println("Not a valid directory, please try again");
+public static void main(String[] args) {
+	Scanner scanny = null;
+	try {
+	    scanny = new Scanner(new File("/Users/qbp/Dropbox/WT 2017/test.csv"));
+	} catch (FileNotFoundException e) {
+	    System.out.println("Not a valid file:" + e.getMessage());
+	    System.exit(1);
+	}
 
-	    System.exit(0);
-      }
+	ArrayList<MidiArray> arrayArray = reader(scanny);
+	scanny.close();
+
+  for (int i = 0; i < arrayArray.size(); i++) {
+	    System.out.println(arrayArray.get(i).toString());
+	}
+
     }
-    
-    private static ArrayList<MidiArray> reader(Scanner scanny) {
+
+private static ArrayList<MidiArray> reader(Scanner scanny) {
 	ArrayList<MidiArray> arrayArray = new ArrayList<MidiArray>();
 
 	int time = 0;
@@ -98,12 +41,12 @@ public class MidiConv {
 	    int currentTime = Integer.parseInt(currentEvent[1]);
 
 	    //keeps track of tempo as a a global variable
-	    if (currentEvent[2].equals("Tempo") && currentEvent[1].equals("0")) {
+	if (currentEvent[2].equals("Tempo") && currentEvent[1].equals("0")) {
 		TEMPO = Integer.parseInt(currentEvent[3]);
 	    }
 
 	  //keeps track of tempo as a a global variable
-	    if (currentEvent[2].equals("Header")) {
+	if (currentEvent[2].equals("Header")) {
 		STEP = Integer.parseInt(currentEvent[5]) / 4;
 	    }
 
@@ -119,11 +62,12 @@ public class MidiConv {
 		    arrayArray.add(midiObject);
 		}
 
-		/*for (int i = 0; i < currentEvent.length; i++) {
+/*
+		for (int i = 0; i < currentEvent.length; i++) {
 		    System.out.print(currentEvent[i] + " ");
 		}
 		System.out.println("\n");
-		*/
+*/
 
 
 		if (currentEvent[2].equals("Note_on_c")){
@@ -147,7 +91,7 @@ public class MidiConv {
 	return parts;
     }
 
-    public static class MidiArray {
+public static class MidiArray {
 	public Integer[] notes;
 	public int timing;
 
--- tf-test.py
+++ tf-test.py
@@ -1,34 +1,91 @@
 import tensorflow as tf
+import sys
+import numpy as np
 
 n_input = 127
 n_output = 127
 n_hidden1 = 1024
 
-x = tf.placeholder(tf.float32, [None, n_input])
-# input matrix of 1x127 vectors (from CSV)
-y_ = tf.placeholder(tf.float32, [None, n_output])
-# storing 'correct' answer
+def main():
+    arglist = sys.argv
+    if len(arglist) < 2: # 1D10T error
+        print("No input file given.")
+        sys.exit()
 
-W_hidden1 = tf.Variable(tf.zeros([n_input, n_hidden1]))
-b_hidden1 = tf.Variable(tf.zeros([n_hidden]))
-# weights and biases of hidden layer 1
+    # The following code sets up the network as desired,
+    # followed by data setup
 
-out_hidden1 = tf.nn.sigmoid(tf.matmul(x, W_hidden1) + b)
+    x = tf.placeholder(tf.float32, [None, n_input])
+    # input matrix of 1x127 vectors (from CSV)
+    y_ = tf.placeholder(tf.float32, [None, n_output])
+    # storing 'correct' answer
 
-W_out = tf.Variable(tf.zeros([n_hidden1, n_output]))
-b_out = tf.Variable(tf.zeros([n_output]))
-# weights and biases of output layer
+    W_hidden1 = tf.Variable(tf.zeros([n_input, n_hidden1]))
+    b_hidden1 = tf.Variable(tf.zeros([n_hidden1]))
+    # weights and biases of hidden layer 1
 
-output = tf.nn.sigmoid(tf.matmul(out_hidden1, W_out) + b_out)
-# normalization
+    out_hidden1 = tf.nn.sigmoid(tf.matmul(x, W_hidden1) + b_hidden1)
 
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-# cost function
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-# optimizer reducing cost function
+    W_out = tf.Variable(tf.zeros([n_hidden1, n_output]))
+    b_out = tf.Variable(tf.zeros([n_output]))
+    # weights and biases of output layer
 
-init = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(init)
+    output = tf.nn.sigmoid(tf.matmul(out_hidden1, W_out) + b_out)
+    # normalization
 
-## relu, sigmoid
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(output), reduction_indices=[1]))
+    # cost function
+    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+    # optimizer reducing cost function
+
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
+
+    # get data
+    fp = open(str(arglist[1]), "r")
+    tuple_array = []
+    for line in fp:
+        tuple_array.append(make_tolerable(line))
+
+    data_array_inorder = []
+    for i in tuple_array:
+        data_array_inorder.append(i[1])
+
+    # run data
+    idx = 0
+    while (idx < len(data_array_inorder) - 1):
+        cost, output = sess.run([cross_entropy, output],
+                        feed_dict={
+                            x:data_array_inorder[idx],
+                            y_:data_array_inorder[idx+1]
+                        })
+
+def make_tolerable(line):
+    # separate tempo from array
+    parts = line.split("[")
+
+    #initialize return val and put tempo in it
+    ret_tuple = []
+    ret_tuple.append(int(parts[0]))
+
+    # init int-ed ret_array
+    ret_array = []
+    values = parts[1].split(", ")
+    for i in range(0, len(values) - 2):
+        ret_array.append(int(values[i]))
+
+    # cast every item in array as int 1 or 0
+    for i in range(0, len(ret_array) - 1):
+        ret_array[i] = int(ret_array[i])
+
+    ret_tuple.append(ret_array)
+
+    # ret[0] is timimg data
+    # ret[1] is inted array
+    return ret_tuple
+
+
+
+if __name__ == "__main__":
+    main()
