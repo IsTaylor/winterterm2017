575759d7b45230a84119188bdcb737324a3d4e80
Isabel Taylor <birdysbox2239@gmail.com>
2017-05-05 10:21:28 -0400

    everything put together correctly and not dealing with github merge issues

---
+++ csvToMidi.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# A test script to run a program on many files
+
+DIRECTORY=""
+while [ ! -d "$DIRECTORY" ]; do
+echo -e "Please enter the folder you've placed your Csv files in:"
+read directory
+DIRECTORY=$directory
+if [ ! -d "$DIRECTORY" ]; then
+  echo "$DIRECTORY is not a directory"
+fi
+done
+
+ORGDIR=$DIRECTORY
+
+mkdir -p ./Midi_final
+
+counter=0
+shopt -s nullglob
+for file in $ORGDIR/*.csv
+do
+  echo -e "heyo"
+  csvmidi "$file" "./Midi_final/$counter.mid"
+  counter=$((counter+1))
+done
+shopt -u nullglob
